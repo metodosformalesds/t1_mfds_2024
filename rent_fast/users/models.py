2214a65b3a0bbb26173bbeef7c46e4a077ca350c
JesusQuinonez204032 <100110866+JesusQuinonez204032@users.noreply.github.com>
2024-10-14 07:32:10 -0600

    Agregué la clase usuario basico en models.py

--- rent_fast/users/models.py
+++ rent_fast/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+username = models.CharField(max_length=150, unique=True)
+email = models.EmailField(unique=True)
+first_name = models.CharField(max_length=30)
+last_name = models.CharField(max_length=30)= models.DateTimeField(auto_now_add=True)
+def __str__(self):
+return self.username
+
