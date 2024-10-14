c54fe492c92665fd35c6448d12295df3abe39362
ManuelVillarreal10 <143544530+ManuelVillarreal10@users.noreply.github.com>
2024-10-14 14:28:24 -0600

    Update models.py

--- rent_fast/users/models.py
+++ rent_fast/users/models.py
@@ -26,4 +26,16 @@ class Arrendador(models.Model):
 {self.is_verified}"
 
 
+ from django.db import models
+ from django.contrib.auth.models import User # Importa el modelo
+ base de usuario
+ class Arrendatario(models.Model):
+ user = models.OneToOneField(User, on_delete=models.CASCADE)
+ telefono = models.CharField(max_length=15)
+ direccion = models.CharField(max_length=255)
+ is_verified = models.BooleanField(default=False) # Verificación
+ del arrendatario
+ def __str__(self):
+ return f"{self.user.username}- Verificado:
+ {self.is_verified}"
 
