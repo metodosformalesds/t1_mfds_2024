8614db354a4f8d932ce7284ee5f2a28bc23ec2b0
JuanCarlosFloresSanchez-214720 <143544618+JuanCarlosFloresSanchez-214720@users.noreply.github.com>
2024-10-14 10:34:50 -0600

    Agregué la clase Arrendador en models.py

--- rent_fast/users/models.py
+++ rent_fast/users/models.py
@@ -9,3 +9,21 @@ last_name = models.CharField(max_length=30)= models.DateTimeField(auto_now_add=T
 def __str__(self):
 return self.username
 
+# Usuario Arrendador
+
+from django.db import models
+from django.contrib.auth.models import User 
+# Importa el modelo base de usuario
+class Arrendador(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) 
+#Relación con el modelo User
+    telefono = models.CharField(max_length=15)
+    direccion = models.CharField(max_length=255)
+    is_verified = models.BooleanField(default=False) 
+# Verificación del arrendador
+    def __str__(self):
+         return f"{self.user.username} - Verificado:
+{self.is_verified}"
+
+
+
