0925ea775730a937fcdc4f7356b369ebe11f249a
JesusQuinonez204032 <100110866+JesusQuinonez204032@users.noreply.github.com>
2024-11-07 14:59:25 -0700

    formulario de preguntas

--- rent_fast/rentas/forms.py
+++ rent_fast/rentas/forms.py
@@ -1,6 +1,19 @@
 # chat/forms.py
 from django import forms
 from .models import Mensaje
+from .models import Pregunta, Respuesta
+
+class PreguntaForm(forms.ModelForm):
+    class Meta:
+        model = Pregunta
+        fields = ['pregunta_texto']
+        labels = {'pregunta_texto': 'Tu pregunta'}
+
+class RespuestaForm(forms.ModelForm):
+    class Meta:
+        model = Respuesta
+        fields = ['respuesta_texto']
+        labels = {'respuesta_texto': 'Responder'}
 
 class MensajeForm(forms.ModelForm):
     class Meta:
