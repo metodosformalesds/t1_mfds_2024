286f12b5ce465fad9055b1f53f2c0e419119ad5f
David-P3FES <al215814@alumnos.uacj.mx>
2024-11-12 14:58:18 -0600

    implementacionFigma

--- rent_fast/users/context_processors.py
+++ rent_fast/users/context_processors.py
@@ -1,7 +1,16 @@
 from .models import Notificacion
+from django.conf import settings
 
 def notificaciones_no_leidas(request):
     if request.user.is_authenticated:
         unread_count = Notificacion.objects.filter(usuario=request.user, leido=False).count()
         return {'unread_count': unread_count}
     return {'unread_count': 0}
+
+
+def cart_count(request):
+    if request.user.is_authenticated:
+        cart_count = request.user.cart.items.count()
+    else:
+        cart_count = 0
+    return {'cart_count': cart_count}
