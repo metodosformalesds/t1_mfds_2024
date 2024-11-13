58cd4c0d667fa9f5a4827710aadee543138af1d7
eduardani84 <eduardani84@gmail.com>
2024-11-13 15:00:23 +0000

    notifications fixed and cart fixed

--- rent_fast/users/context_processors.py
+++ rent_fast/users/context_processors.py
@@ -1,11 +1,26 @@
 from .models import Notificacion
 from django.conf import settings
 
+from .models import Notificacion
+from django.conf import settings
+
 def notificaciones_no_leidas(request):
     if request.user.is_authenticated:
+        # Obtener el conteo de notificaciones no leídas
         unread_count = Notificacion.objects.filter(usuario=request.user, leido=False).count()
-        return {'unread_count': unread_count}
-    return {'unread_count': 0}
+        
+        # Obtener las notificaciones no leídas
+        notificaciones_no_leidas = Notificacion.objects.filter(usuario=request.user, leido=False)
+        
+        # Enviar tanto el contador como las notificaciones
+        return {
+            'unread_count': unread_count,
+            'notificaciones_no_leidas': notificaciones_no_leidas
+        }
+    
+    # Si no está autenticado, retornar solo el contador en 0
+    return {'unread_count': 0, 'notificaciones_no_leidas': []}
+
 
 
 def cart_count(request):
