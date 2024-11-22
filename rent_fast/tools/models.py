f91c1ed1d0dc0672993259ef4c314f0fa4853844
ManuelVillarreal10 <143544530+ManuelVillarreal10@users.noreply.github.com>
2024-11-22 00:22:08 -0600

    Docu models_py

--- rent_fast/tools/models.py
+++ rent_fast/tools/models.py
@@ -4,12 +4,38 @@ from rentas.models import Renta
 from datetime import timedelta  
 
 class Categoria(models.Model):
+    """
+    Representa una categoría a la que puede pertenecer una herramienta.
+
+    Atributos:
+    - nombre (str): El nombre de la categoría de la herramienta. Este campo es único.
+
+    Métodos:
+    - __str__(): Devuelve el nombre de la categoría como representación de cadena.
+    """
     nombre = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.nombre
         
 class Tool(models.Model):
+    """
+    Representa una herramienta disponible para ser alquilada por un arrendatario.
+
+    Atributos:
+    - arrendador (ForeignKey): El arrendador que posee la herramienta.
+    - nombre (str): Nombre de la herramienta.
+    - descripcion (TextField): Descripción detallada de la herramienta.
+    - costo_dia (DecimalField): Costo de alquiler de la herramienta por día.
+    - estado (str): Estado de la herramienta. Puede ser 'Pendiente', 'Disponible', o 'Rechazado'.
+    - imagenes (ImageField): Imágenes de la herramienta.
+    - categoria (ForeignKey): Categoría a la que pertenece la herramienta.
+    - mensaje_rechazo (TextField): Motivo por el cual la herramienta fue rechazada, si aplica.
+
+    Métodos:
+    - __str__(): Devuelve el nombre de la herramienta como representación de cadena.
+    - is_available(): Verifica si la herramienta está disponible para ser alquilada. 
+    """
     arrendador = models.ForeignKey(Arrendador, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=255)
     descripcion = models.TextField()
@@ -32,6 +58,20 @@ class Tool(models.Model):
 
         
 class Carrito(models.Model):
+    """
+    Representa un elemento en el carrito de un arrendatario.
+
+    Atributos:
+    - arrendatario (ForeignKey): El arrendatario que tiene la herramienta en su carrito.
+    - herramienta (ForeignKey): La herramienta que se encuentra en el carrito.
+    - fecha_agregada (DateTimeField): Fecha y hora en la que la herramienta fue agregada al carrito.
+    - fecha_inicio (DateField): Fecha de inicio de la renta de la herramienta.
+    - fecha_fin (DateField): Fecha de finalización de la renta de la herramienta.
+    - costo_total (DecimalField): Costo total de la renta calculado en función de las fechas.
+
+    Métodos:
+    - __str__(): Devuelve una representación de cadena del carrito, mostrando el nombre de la herramienta y el arrendatario.
+    """
     arrendatario = models.ForeignKey(Arrendatario, on_delete=models.CASCADE)
     herramienta = models.ForeignKey('Tool', on_delete=models.CASCADE)
     fecha_agregada = models.DateTimeField(auto_now_add=True)
