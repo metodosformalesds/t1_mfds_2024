5ea6be9db5ffb6afc05feb526bd67162a66cff05
ManuelVillarreal10 <143544530+ManuelVillarreal10@users.noreply.github.com>
2024-11-22 13:24:09 -0600

    doc routing.py

--- rent_fast/users/templates/users/routing.py
+++ rent_fast/users/templates/users/routing.py
@@ -1,7 +1,12 @@
 # routing.py
 from django.urls import path
 from . import consumers
+# Define las rutas WebSocket para la aplicación.
+# Este archivo mapea las URLs de WebSocket a los consumidores correspondientes.
+
 
 websocket_urlpatterns = [
+# Ruta WebSocket para la verificación de identidad del usuario.
+# La URL contiene un parámetro `user_id` que se pasa al consumidor para identificar al usuario.
     path("ws/verify_identity/<str:user_id>/", consumers.IdentityConsumer.as_asgi()),
 ]
