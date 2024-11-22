7765d16cb75b597c1e2405f6f638a121b09c6bbf
ManuelVillarreal10 <143544530+ManuelVillarreal10@users.noreply.github.com>
2024-11-22 00:05:29 -0600

    Documentacion paypal_Urls-py

--- rent_fast/rentas/paypal.py
+++ rent_fast/rentas/paypal.py
@@ -8,3 +8,17 @@ paypalrestsdk.configure({
     "client_id": settings.PAYPAL_CLIENT_ID,
     "client_secret": settings.PAYPAL_CLIENT_SECRET,
 })
+"""
+Este archivo contiene la configuración inicial del SDK de PayPal para el sistema de pagos.
+
+El SDK de PayPal se configura con los detalles proporcionados en las configuraciones del proyecto, 
+como el modo (sandbox o live), el client_id y el client_secret.
+
+Dependencias:
+- paypalrestsdk: Biblioteca de PayPal para interactuar con su API.
+- settings: Archivo de configuración de Django que contiene las credenciales de PayPal.
+
+Proceso de configuración:
+1. Configura el entorno de PayPal (modo de pruebas o producción).
+2. Configura las credenciales de PayPal, como el ID de cliente y el secreto del cliente.
+"""
--- rent_fast/rentas/urls.py
+++ rent_fast/rentas/urls.py
@@ -3,6 +3,41 @@ from django.urls import path
 from .views import soporte_view,iniciar_pago_view,preguntas_sin_responder_view, pago_confirmacion_view, pago_exitoso_view, pago_cancelado_view, ver_chat_view, listar_chats_view, rentas_arrendador_view, rentas_arrendatario_view, finalizar_renta_view, ocultar_chat, restaurar_chat_view, mostrar_chat
 from django.conf import settings
 from django.conf.urls.static import static
+"""
+Este archivo define las URL para las vistas de la aplicación de Rentas.
+
+A través de estas URL, los usuarios pueden acceder a diferentes funcionalidades, como el inicio de pagos, 
+confirmación de pagos, visualización de chats, listado de rentas y soporte.
+
+URLs definidas:
+
+1. Pago y Confirmaciones:
+   - /pago/ : Inicia el proceso de pago.
+   - /pago/exitoso/ : Vista para cuando un pago se ha realizado con éxito.
+   - /pago/cancelado/ : Vista para cuando un pago ha sido cancelado.
+   - /pago/confirmacion/ : Vista para confirmar un pago.
+
+2. Chats:
+   - /<chat_id>/ : Muestra el chat específico de un arrendador y arrendatario.
+   - /chats/ : Lista todos los chats disponibles.
+   - /ocultar_chat/<chat_id>/ : Oculta un chat.
+   - /restaurar_chat/<chat_id>/ : Restaura un chat oculto.
+   - /chats/mostrar/<chat_id>/ : Muestra el contenido del chat.
+
+3. Rentas:
+   - /rentas_arrendador/ : Vista de rentas para arrendadores.
+   - /rentas_arrendatario/ : Vista de rentas para arrendatarios.
+   - /finalizar-renta/<renta_id>/ : Finaliza una renta específica.
+
+4. Preguntas:
+   - /preguntas-sin-responder/ : Muestra las preguntas de los arrendatarios que aún no han sido respondidas.
+
+5. Soporte:
+   - /soporte/ : Vista para contactar con soporte técnico.
+
+Dependencias:
+- settings: Utiliza las configuraciones de `MEDIA_URL` y `MEDIA_ROOT` para servir archivos estáticos.
+"""
 
 urlpatterns = [
     path("pago/", iniciar_pago_view, name="iniciar_pago"),
