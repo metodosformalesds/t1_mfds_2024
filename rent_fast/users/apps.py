24a7e032e64807500298e3560a9d2b3402a5a72d
ManuelVillarreal10 <143544530+ManuelVillarreal10@users.noreply.github.com>
2024-11-22 01:26:53 -0600

    Doc apps_py

--- rent_fast/users/apps.py
+++ rent_fast/users/apps.py
@@ -2,5 +2,15 @@ from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
+    """
+    Configuración de la aplicación Users en Django.
+
+    Esta clase define la configuración de la aplicación 'users', que es responsable de gestionar
+    los usuarios del sistema, incluyendo arrendadores y arrendatarios.
+
+    Atributos:
+    - default_auto_field: Define el tipo de campo para las claves primarias (en este caso, BigAutoField).
+    - name: El nombre completo de la aplicación, que debe coincidir con el nombre de la carpeta de la app.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
