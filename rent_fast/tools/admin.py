e6db7b50aca731ef503d0d1eb8eb6da282d1c2e1
ManuelVillarreal10 <143544530+ManuelVillarreal10@users.noreply.github.com>
2024-11-22 00:13:30 -0600

    Documentacion admin_py

--- rent_fast/tools/admin.py
+++ rent_fast/tools/admin.py
@@ -2,11 +2,34 @@ from django.contrib import admin
 from .models import Tool, Renta
 
 class ToolAdmin(admin.ModelAdmin):
+    """
+    Configuración para la administración del modelo Tool en el panel de administración de Django.
+
+    Atributos:
+    model (Tool): El modelo asociado a esta clase de administración.
+    list_display (list): Campos a mostrar en la lista de herramientas dentro del panel de administración.
+    search_fields (list): Campos por los cuales se podrá buscar al realizar consultas en el panel de administración.
+
+    Campos visibles:
+    - nombre: El nombre de la herramienta.
+    - costo_dia: El costo de la herramienta por día.
+    - estado: El estado de la herramienta (disponible o no).
+    """  
     model = Tool
     list_display = ["nombre", "costo_dia", "estado"]
     search_fields = ["nombre", "descripcion"]
 
 class RentaAdmin(admin.ModelAdmin):
+    """
+    Configuración para la administración del modelo Renta en el panel de administración de Django.
+
+    Atributos:
+    model (Renta): El modelo asociado a esta clase de administración.
+    list_display (list): Campos a mostrar en la lista de rentas dentro del panel de administración.
+
+    Campos visibles:
+    - herramienta: La herramienta asociada con la renta.
+    """
     model = Renta
     list_display = ["herramienta"]
 
